3bb5e4b1f11ced28da8d2c910814e6df5327eb57
LenovoPC <ssebas@gmail.com>
2021-08-04 19:26:43 -0300

    Se agrega el proyecto

---
+++ js/app.js
@@ -0,0 +1,45 @@
+console.log('Aplicacion Calculadora');
+
+function sumar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operacion no incluye numeros';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log()
+}
+
+function restar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operacion no incluye numeros';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log()
+}
+
+function dividir(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operacion no incluye numeros';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log()
+}
+
+function multiplicar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operacion no incluye numeros';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log()
+}
